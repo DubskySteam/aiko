df898e127fb7d330944100e88b654fe67bed94e8
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2025-02-27 12:02:14 +0100

    window now starts in 2 different resolutions depending on the monitor resolution

--- src/main/kotlin/dev/dubsky/aiko/Entry.kt
+++ src/main/kotlin/dev/dubsky/aiko/Entry.kt
@@ -4,16 +4,26 @@ import androidx.compose.foundation.window.WindowDraggableArea
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.isCtrlPressed
 import androidx.compose.ui.input.key.key
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.*
 import dev.dubsky.aiko.screens.Composer
+import java.awt.Toolkit
 
 fun main() = application {
+    var screenSize = Toolkit.getDefaultToolkit().screenSize
+    var screenWidth = if (screenSize.width >= 2560) 1920.dp else 1280.dp
+    var screenHeight = if (screenSize.height >= 1440) 1080.dp else 720.dp
+
     Window(
         onCloseRequest = ::exitApplication,
         title = "Aiko",
         undecorated = true,
         resizable = true,
+        state = WindowState(
+            placement = WindowPlacement.Floating,
+            size = DpSize(screenWidth, screenHeight),
+        ),
         onKeyEvent = {
             if (it.key == Key.X && it.isCtrlPressed) {
                 exitApplication()
