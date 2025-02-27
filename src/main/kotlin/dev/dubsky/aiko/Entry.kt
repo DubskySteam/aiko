7747df7028f602365d03e700a185355d8383ddb3
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2025-02-27 11:39:57 +0100

    added ctrl+x shortcut to close window

--- src/main/kotlin/dev/dubsky/aiko/Entry.kt
+++ src/main/kotlin/dev/dubsky/aiko/Entry.kt
@@ -1,6 +1,9 @@
 package dev.dubsky.aiko
 
 import androidx.compose.foundation.window.WindowDraggableArea
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.isCtrlPressed
+import androidx.compose.ui.input.key.key
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import dev.dubsky.aiko.screens.Composer
@@ -10,7 +13,16 @@ fun main() = application {
         onCloseRequest = ::exitApplication,
         title = "Aiko",
         undecorated = true,
-        resizable = true
+        resizable = true,
+        onKeyEvent = {
+            if (it.key == Key.X && it.isCtrlPressed) {
+                exitApplication()
+                true
+            }
+            else {
+                false
+            }
+        }
     ) {
         WindowDraggableArea {
             Composer()
