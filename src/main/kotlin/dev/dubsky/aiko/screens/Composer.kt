4e803ce85f03968e0557b683512e5d65a0f1430e
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2025-02-27 13:40:10 +0100

    fix Navbar not sticking to the bottom

--- src/main/kotlin/dev/dubsky/aiko/screens/Composer.kt
+++ src/main/kotlin/dev/dubsky/aiko/screens/Composer.kt
@@ -1,8 +1,10 @@
 package dev.dubsky.aiko.screens
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.WindowState
 import dev.dubsky.aiko.components.bar.ControlBar
@@ -11,34 +13,44 @@ import dev.dubsky.aiko.components.bar.NavBar
 @Preview
 @Composable
 fun Composer() {
-    var screenActive by remember { mutableStateOf(Screens.Home) }
+    var screenActive by remember { mutableStateOf(Screens.List) }
     val windowState by remember { mutableStateOf(WindowState()) }
 
-    Column {
-        ControlBar(
-            windowState = windowState,
-            onMinimizeClick = {
-                windowState.isMinimized = true
-            },
-            onMaximizeClick = {
-            },
-            onCloseClick = {
-                System.exit(0)
-            }
-        )
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            ControlBar(
+                windowState = windowState,
+                onMinimizeClick = {
+                    windowState.isMinimized = true
+                },
+                onMaximizeClick = {
+                },
+                onCloseClick = {
+                    System.exit(0)
+                }
+            )
 
-        when (screenActive) {
-            Screens.Home -> HomeScreen()
-            Screens.Browse -> ComingScreen()
-            Screens.Player -> ComingScreen()
-            Screens.List -> ComingScreen()
-            Screens.Settings -> ComingScreen()
+            when (screenActive) {
+                Screens.Home -> HomeScreen()
+                Screens.Browse -> ComingScreen()
+                Screens.Player -> ComingScreen()
+                Screens.List -> ComingScreen()
+                Screens.Settings -> ComingScreen()
+            }
         }
 
-        NavBar(
-            currentScreen = screenActive,
-            onScreenSelected = { screenActive = it },
-            menuSize = 56.dp
-        )
+        Box(
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) {
+            NavBar(
+                currentScreen = screenActive,
+                onScreenSelected = { screenActive = it },
+                menuSize = 56.dp
+            )
+        }
     }
 }
