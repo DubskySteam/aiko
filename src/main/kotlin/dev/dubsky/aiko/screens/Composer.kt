b3acab1650fbdc79bf06729c89538cdb7c461180
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2025-03-03 20:36:20 +0100

    CORE: Composer now refers to AnimeScreen through a onClick passtrough

--- src/main/kotlin/dev/dubsky/aiko/screens/Composer.kt
+++ src/main/kotlin/dev/dubsky/aiko/screens/Composer.kt
@@ -10,12 +10,14 @@ import androidx.compose.ui.window.WindowState
 import dev.dubsky.aiko.api.AnimeData
 import dev.dubsky.aiko.components.bar.ControlBar
 import dev.dubsky.aiko.components.bar.NavBar
+import dev.dubsky.aiko.data.Anime
 
 @Preview
 @Composable
-fun Composer() {
-    var screenActive by remember { mutableStateOf(Screens.Home) }
-    val windowState by remember { mutableStateOf(WindowState()) }
+fun Composer(windowState: WindowState) {
+    var screenActive by remember { mutableStateOf(Screens.Settings) }
+    var selectedAnime by remember { mutableStateOf<Anime?>(null) }
+    val windowState by remember { mutableStateOf(windowState) }
 
     Box(
         modifier = Modifier.fillMaxSize()
@@ -37,12 +39,20 @@ fun Composer() {
 
             when (screenActive) {
                 Screens.Home -> {
-                    HomeScreen()
+                    HomeScreen(
+                        onAnimeSelected = {
+                            selectedAnime = it
+                            screenActive = Screens.Anime
+                        }
+                    )
+                }
+                Screens.Anime -> {
+                    selectedAnime?.let { AnimeScreen(anime = it) }
                 }
                 Screens.Browse -> ComingScreen()
                 Screens.Player -> ComingScreen()
                 Screens.List -> ComingScreen()
-                Screens.Settings -> SettingsScreen()
+                Screens.Settings -> SettingsScreen(windowState)
             }
         }
 
